9faadb9252473564a294f075ad717109d6edbd4f
ViniciusEckert <viniciuseckert6@gmail.com>
2025-11-03 11:29:58 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import styles from'./App.module.css'
+import fotoPerfil from '../public/foto1.png'
 
 function App() {
 
@@ -11,19 +12,29 @@ function App() {
     <a href="#s4">Contato</a>
    </nav>
    <main>
-    <section>
-      <h2>sessao 1</h2>
+    <section className={styles.s1} id="s1">
+      <div className={styles.left}>
+        <img className={styles.imgPerfil} src={fotoPerfil} alt="Eu" />
+      </div>
+      <div className={styles.right}>
+        <h2 className={styles.title}>Vinicius Eckert</h2>
+        <p className={styles.paragraph}>Aluno do curso tecnico de desenvolvimento de sistemas, terminando o 3ºano do ensino medio </p>
+      </div>
     </section>
-    <section>
+
+    <section id="s2">
       <h2>sessao 2</h2>
     </section>
-    <section>
+
+    <section id="s3">
       <h2>sessao 3</h2>
     </section>
-    <section>
+
+    <section id="s4">
       <h2>sessao 4</h2>
     </section>
    </main>
+
    </>
   )
 }
